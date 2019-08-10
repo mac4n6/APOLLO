649210ca6fcda4465e4c05c9469a095b8d231c0f
Brigs <abrignoni@gmail.com>
2019-08-10 16:10:46 -0400

    Add files via upload
    Added error handling when no usagestats directory is found

--- artemis.py
+++ artemis.py
@@ -19,12 +19,15 @@ mod_dir = args.modules_directory
 data_dir = args.data_dir_to_analyze
 
 #Search for the usagestats directory
+#initialize var
+pathfound = 0
 
 for root, dirs, filenames in os.walk(data_dir):
 		for f in dirs:
 			if f == "usagestats":
 				pathfound = os.path.join(root, f)
-
+				print(pathfound)
+			
 #Create sqlite databases
 db = sqlite3.connect(data_dir+r'\usagestats.db')
 cursor = db.cursor()
@@ -56,132 +59,136 @@ print("-------------------------------------------------------------------------
 print("Locating Usagestats XML files + processing")
 print("Please wait...") 
 
-for filename in glob.iglob(pathfound+'\**', recursive=True):
-	if os.path.isfile(filename): # filter dirs
-		file_name = os.path.splitext(os.path.basename(filename))[0]
-		#Test if xml is well formed
-		if file_name == 'version':
-			continue	
-		else:
-			if 'daily' in filename:
-				sourced = 'daily'
-			elif 'weekly' in filename:
-				sourced = 'weekly'
-			elif 'monthly' in filename:
-				sourced = 'monthly'
-			elif 'yearly' in filename:
-				sourced = 'yearly'
-			
-			try:
-				file_name_int = int(file_name)
-			except: 
-				print('Invalid filename: ')
-				print(filename)
-				print('')
-				err = 1
-			
-			try:
-				ET.parse(filename)
-			except ET.ParseError:
-				print('Parse error - Non XML file? at: ')
-				print(filename)
-				print('')
-				err = 1
-				#print(filename)
-			
-			if err == 1:
-				err = 0
-				continue
+if pathfound == 0:
+	print("No UsageStats XML directory located")
+else:
+
+	for filename in glob.iglob(pathfound+'\**', recursive=True):
+		if os.path.isfile(filename): # filter dirs
+			file_name = os.path.splitext(os.path.basename(filename))[0]
+			#Test if xml is well formed
+			if file_name == 'version':
+				continue	
 			else:
-				tree = ET.parse(filename)
-				root = tree.getroot()
-				print('Processing: '+filename)
-				print('')
-				for elem in root:
-					#print(elem.tag)
-					usagetype = elem.tag
-					#print("Usage type: "+usagetype)
-					if usagetype == 'packages':
-						for subelem in elem:
-							#print(subelem.attrib)
-							fullatti_str = json.dumps(subelem.attrib)
-							#print(subelem.attrib['lastTimeActive'])
-							time1 = subelem.attrib['lastTimeActive']
-							time1 = int(time1)
-							if time1 < 0:
-								finalt = abs(time1)
-							else:
-								finalt = file_name_int + time1
-							#print('final time: ')
-							#print(finalt)
-							#print(subelem.attrib['package'])
-							pkg = (subelem.attrib['package'])
-							#print(subelem.attrib['timeActive'])
-							tac = (subelem.attrib['timeActive'])
-							#print(subelem.attrib['lastEvent'])
-							#insert in database
-							cursor = db.cursor()
-							datainsert = (usagetype, finalt, tac, pkg, '' , '' , sourced, fullatti_str,)
-							#print(datainsert)
-							cursor.execute('INSERT INTO data (usage_type, lastime, timeactive, package, types, classs, source, fullatt)  VALUES(?,?,?,?,?,?,?,?)', datainsert)
-							db.commit()
-					
-					elif usagetype == 'configurations':
-						for subelem in elem:
-							fullatti_str = json.dumps(subelem.attrib)
-							#print(subelem.attrib['lastTimeActive'])
-							time1 = subelem.attrib['lastTimeActive']
-							time1 = int(time1)
-							if time1 < 0:
-								finalt = abs(time1)
-							else:
-								finalt = file_name_int + time1
-							#print('final time: ')
-							#print(finalt)
-							#print(subelem.attrib['timeActive'])
-							tac = (subelem.attrib['timeActive'])
-							#print(subelem.attrib)
-							#insert in database
-							cursor = db.cursor()
-							datainsert = (usagetype, finalt, tac, '' , '' , '' , sourced, fullatti_str,)
-							cursor.execute('INSERT INTO data (usage_type, lastime, timeactive, package, types, classs, source, fullatt)  VALUES(?,?,?,?,?,?,?,?)', datainsert)
-							db.commit()
-			
-					elif usagetype == 'event-log':
-						for subelem in elem:
-							#print(subelem.attrib['time'])
-							time1 = subelem.attrib['time']
-							time1 = int(time1)
-							if time1 < 0:
-								finalt = abs(time1)
-							else:
-								finalt = file_name_int + time1
-							
-							#time1 = subelem.attrib['time']
-							#finalt = file_name_int + int(time1)
-							#print('final time: ')
-							#print(finalt)
-							#print(subelem.attrib['package'])
-							pkg = (subelem.attrib['package'])
-							#print(subelem.attrib['type'])
-							tipes = (subelem.attrib['type'])
-							#print(subelem.attrib)
-							fullatti_str = json.dumps(subelem.attrib)
-							#add variable for type conversion from number to text explanation
-							#print(subelem.attrib['fs'])
-							#classy = subelem.attrib['class']
-							if 'class' in subelem.attrib:
-								classy = subelem.attrib['class']
+				if 'daily' in filename:
+					sourced = 'daily'
+				elif 'weekly' in filename:
+					sourced = 'weekly'
+				elif 'monthly' in filename:
+					sourced = 'monthly'
+				elif 'yearly' in filename:
+					sourced = 'yearly'
+				
+				try:
+					file_name_int = int(file_name)
+				except: 
+					print('Invalid filename: ')
+					print(filename)
+					print('')
+					err = 1
+				
+				try:
+					ET.parse(filename)
+				except ET.ParseError:
+					print('Parse error - Non XML file? at: ')
+					print(filename)
+					print('')
+					err = 1
+					#print(filename)
+				
+				if err == 1:
+					err = 0
+					continue
+				else:
+					tree = ET.parse(filename)
+					root = tree.getroot()
+					print('Processing: '+filename)
+					print('')
+					for elem in root:
+						#print(elem.tag)
+						usagetype = elem.tag
+						#print("Usage type: "+usagetype)
+						if usagetype == 'packages':
+							for subelem in elem:
+								#print(subelem.attrib)
+								fullatti_str = json.dumps(subelem.attrib)
+								#print(subelem.attrib['lastTimeActive'])
+								time1 = subelem.attrib['lastTimeActive']
+								time1 = int(time1)
+								if time1 < 0:
+									finalt = abs(time1)
+								else:
+									finalt = file_name_int + time1
+								#print('final time: ')
+								#print(finalt)
+								#print(subelem.attrib['package'])
+								pkg = (subelem.attrib['package'])
+								#print(subelem.attrib['timeActive'])
+								tac = (subelem.attrib['timeActive'])
+								#print(subelem.attrib['lastEvent'])
+								#insert in database
 								cursor = db.cursor()
-								datainsert = (usagetype, finalt, '' , pkg , tipes , classy , sourced, fullatti_str,)
+								datainsert = (usagetype, finalt, tac, pkg, '' , '' , sourced, fullatti_str,)
+								#print(datainsert)
 								cursor.execute('INSERT INTO data (usage_type, lastime, timeactive, package, types, classs, source, fullatt)  VALUES(?,?,?,?,?,?,?,?)', datainsert)
 								db.commit()
-							else:
-							#insert in database
+						
+						elif usagetype == 'configurations':
+							for subelem in elem:
+								fullatti_str = json.dumps(subelem.attrib)
+								#print(subelem.attrib['lastTimeActive'])
+								time1 = subelem.attrib['lastTimeActive']
+								time1 = int(time1)
+								if time1 < 0:
+									finalt = abs(time1)
+								else:
+									finalt = file_name_int + time1
+								#print('final time: ')
+								#print(finalt)
+								#print(subelem.attrib['timeActive'])
+								tac = (subelem.attrib['timeActive'])
+								#print(subelem.attrib)
+								#insert in database
 								cursor = db.cursor()
-								datainsert = (usagetype, finalt, '' , pkg , tipes , '' , sourced, fullatti_str,)
+								datainsert = (usagetype, finalt, tac, '' , '' , '' , sourced, fullatti_str,)
 								cursor.execute('INSERT INTO data (usage_type, lastime, timeactive, package, types, classs, source, fullatt)  VALUES(?,?,?,?,?,?,?,?)', datainsert)
 								db.commit()
+				
+						elif usagetype == 'event-log':
+							for subelem in elem:
+								#print(subelem.attrib['time'])
+								time1 = subelem.attrib['time']
+								time1 = int(time1)
+								if time1 < 0:
+									finalt = abs(time1)
+								else:
+									finalt = file_name_int + time1
+								
+								#time1 = subelem.attrib['time']
+								#finalt = file_name_int + int(time1)
+								#print('final time: ')
+								#print(finalt)
+								#print(subelem.attrib['package'])
+								pkg = (subelem.attrib['package'])
+								#print(subelem.attrib['type'])
+								tipes = (subelem.attrib['type'])
+								#print(subelem.attrib)
+								fullatti_str = json.dumps(subelem.attrib)
+								#add variable for type conversion from number to text explanation
+								#print(subelem.attrib['fs'])
+								#classy = subelem.attrib['class']
+								if 'class' in subelem.attrib:
+									classy = subelem.attrib['class']
+									cursor = db.cursor()
+									datainsert = (usagetype, finalt, '' , pkg , tipes , classy , sourced, fullatti_str,)
+									cursor.execute('INSERT INTO data (usage_type, lastime, timeactive, package, types, classs, source, fullatt)  VALUES(?,?,?,?,?,?,?,?)', datainsert)
+									db.commit()
+								else:
+								#insert in database
+									cursor = db.cursor()
+									datainsert = (usagetype, finalt, '' , pkg , tipes , '' , sourced, fullatti_str,)
+									cursor.execute('INSERT INTO data (usage_type, lastime, timeactive, package, types, classs, source, fullatt)  VALUES(?,?,?,?,?,?,?,?)', datainsert)
+									db.commit()
 
 print("Calling Apollo for database processing...")
 print("Artemis is done. Good bye")
