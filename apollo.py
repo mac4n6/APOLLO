22acbfa8892f89dbb0a4025f14c1656c9c49f4ec
mac4n6 <mac4n6@users.noreply.github.com>
2019-08-11 12:25:41 -0700

    Python3!
    Changed Version

--- apollo.py
+++ apollo.py
@@ -183,10 +183,9 @@ if __name__ == "__main__":
 	\n\nVery lazy parser to extract pattern-of-life data from SQLite databases on iOS/macOS datasets (though really any SQLite database if you make a configuration file and provide it the proper metadata details.\
 	\n\nOutputs include SQLite Database or CSV.\
 	\n\nYolo! Meant to run on anything and everything, like a honey badger - it don't care. Can be used with multiple dumps of devices. It will run all queries in all modules with no regard for versioning. May lead to redundant data since it can run more than one similar query. Be careful with this option.\
-	\n\n\tVersion: BETA 08042019 - TESTING PURPOSES ONLY, SERIOUSLY.\
-	\n\tUpdated: 08/04/2019\
+	\n\n\tVersion: BETA 08112019 - TESTING PURPOSES ONLY, SERIOUSLY.\
+	\n\tUpdated: 08/11/2019\
 	\n\tAuthor: Sarah Edwards | @iamevltwin | mac4n6.com\
-	\n\tAdded Efficiency: Sam Alptekin of @sjc_CyberCrimes"
 		, prog='apollo.py'
 		, formatter_class=RawTextHelpFormatter)
 	parser.add_argument('-o', choices=['sql','csv'], required=True, action="store", help="Output: sql=SQLite or csv=CSV (required)")
